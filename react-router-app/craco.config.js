01b9b4c3cdba337df8e6c8865292582d37b3b0c4
hanosong <838519800@qq.com>
2023-04-13 12:31:49 +0800

    一些小测试

--- react-router-app/craco.config.js
+++ react-router-app/craco.config.js
@@ -1,21 +1,61 @@
+const { getLoader, loaderByName } = require('@craco/craco');
+
 module.exports = {
-    style: {
-        css: {
-            loaderOptions: {
-                modules: {
-                    localIdentName: '[local]_[hash:base64:5]',
-                },
-            },
-        },
-        less: {
-            lessOptions: {
-                modifyVars: {
-                    'primary-color': '#1DA57A',
-                    'link-color': '#1DA57A',
-                    'border-radius-base': '2px',
-                },
-                javascriptEnabled: true,
-            },
+    webpack: {
+        configure: (webpackConfig, { env, paths }) => {
+            const lessExtension = /\.module\.less$/;
+
+            const { isFound: lessModuleFound } = getLoader(
+                webpackConfig,
+                loaderByName('less-loader')
+            );
+
+            if (lessModuleFound) {
+                const { oneOf } = webpackConfig.module.rules.find(
+                    (rule) => typeof rule.oneOf === 'object'
+                );
+
+                if (oneOf) {
+                    oneOf.unshift({
+                        test: lessExtension,
+                        use: [
+                            {
+                                loader: loaderByName('style-loader').loader,
+                            },
+                            {
+                                loader: loaderByName('css-loader').loader,
+                                options: {
+                                    modules: true,
+                                    importLoaders: 2,
+                                },
+                            },
+                            {
+                                loader: loaderByName('postcss-loader').loader,
+                            },
+                            {
+                                loader: loaderByName('less-loader').loader,
+                                options: {
+                                    lessOptions: {
+                                        javascriptEnabled: true,
+                                    },
+                                },
+                            },
+                        ],
+                    });
+                }
+            }
+
+            return webpackConfig;
         },
     },
+    plugins: [
+        {
+            plugin: require('babel-plugin-import'),
+            options: {
+                libraryName: 'antd',
+                libraryDirectory: 'es',
+                style: true,
+            },
+        },
+    ],
 };
--- react-router-app/src/views/home.jsx
+++ react-router-app/src/views/home.jsx
@@ -1,6 +1,27 @@
-import React, { memo } from "react";
+import React, { memo, useEffect, useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 const Home = memo(() => {
+
+  const [fileList, setFileList] = useState({
+    list1: [],
+    list2: [],
+  })
+  useEffect(() => {
+    // 定义在这里，防止每次函数执行时，重新创建fileData造成性能浪费
+    const fileData = [
+      { tempId: '1', name: 'a' },
+      { tempId: '2', name: 'b' },
+    ]
+    fileData.forEach((fileO, index) => {
+      setFileList(fileList => ({
+        ...fileList,
+        [`list${index + 1}`]: [fileO]
+      }))
+    })
+  }, [])
+  useEffect(() => {
+    console.log(fileList, '---')
+  }, [fileList])
   return (
     <div>
       Home
